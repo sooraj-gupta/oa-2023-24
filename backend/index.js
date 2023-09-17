c217590b0c049134a5203f179e904bb08213dd9a
sooraj-gupta <soorajgupta04@gmail.com>
2023-09-17 16:41:27 -0500

    weather prediction

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,77 @@
 import express from "express"
 import cors from "cors"
+import https from "https"
 
 const app = express()
 
-app.use(cors({
-    origin: "http://localhost:3000"
-}))
+// app.use(cors({
+//     origin: "../frontend/",
+// }))
 
-app.get("/", (req, res) => {
-    res.send({
-        x: [1, 2, 3],
-        y: [4, 5, 6],
-        type: "scatter"
-    }).status(200)
+app.use(express.static('../frontend'));
+
+app.get("/api", (req, res) => {
+  // when the frontend requests the /api endpoint, we make a request to the weather api
+  // and send the response back to the frontend, incldue the user-agent header so the
+  // weather api knows who is making the request
+
+  https.get({
+    hostname: "api.weather.gov",
+    path: "/gridpoints/MKX/37,64/forecast/hourly",
+    headers: {
+      "User-Agent": "weather-app"
+    }
+  }, weatherRes => {
+    let data = ""
+    weatherRes.on("data", chunk => {
+      data += chunk
+    })
+    weatherRes.on("end", () => {
+      try {
+        // Parse the received data into a JavaScript object
+        const weatherData = JSON.parse(data);
+
+        //formatted data in the format of {x: [1, 2, 3], y: [4, 5, 4]}
+        //don't set the graph color to black, set the color of the background of the graph to black
+        const formattedData = {
+          x: [],
+          y: [],
+          type: "scatter",
+          mode: "lines",
+          line: {
+            color: "rgb(0,0,150)"
+          },
+          marker: {
+            color: "rgb(0,0,34)"
+          },
+
+          plot_bgcolor:"black",
+          paper_bgcolor:"black",
+          
+        };
+
+        // Loop through the received data and push the data to the formattedData object
+        weatherData.properties.periods.forEach(period => {
+          formattedData.x.push(new Date(period.startTime).toISOString()),
+          formattedData.y.push(period.temperature);
+        });
+        
+
+        // Send the formatted data as a JSON response to the frontend
+        res.json(formattedData);
+      } catch (error) {
+        // Handle any parsing or formatting errors and send an appropriate response
+        res.status(500).json({ error: "An error occurred while processing the data." });
+      }
+    })
+  })
 })
-
+  
+  // res.send({
+  //   x: [1, 2, 3],
+  //   y: [4, 5, 4],
+  // }).status(200) 
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`server listening on port ${port} 🚀`))
+
--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -2,6 +2,7 @@ const graphDiv = document.getElementById("graph");
 
 fetch(
     "https://oa-7dgb.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+    // "http://localhost:3000/api"
 ).then(async res => {
     Plotly.newPlot( graphDiv, [ await res.json() ]); 
 })
